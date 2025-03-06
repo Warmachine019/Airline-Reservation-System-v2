6f960f1cbbc45f6ca71e127e32c81dd8222f8528
Spandan <91448111+Warmachine019@users.noreply.github.com>
2025-03-06 22:05:36 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli('localhost', 'root', 'root', 'airline');
+$conn = new mysqli('localhost', 'MYSQL_USERNAME_HERE', 'MYSQL_PASSWORD_HERE', 'airline');
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
